cd9a393286a351bce53387f7606f2e96582ce10d
Pipjoy <162532591+pipjoy@users.noreply.github.com>
2025-07-22 10:56:35 -0300

    Add trailing newline to urls.py

--- normapy/urls.py
+++ normapy/urls.py
@@ -18,4 +18,4 @@ urlpatterns = [
 
 # Servir archivos media y estáticos en desarrollo
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
